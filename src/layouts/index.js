d3204ba3617a4843a1fb1748069208884c72a516
Tomi Takussaari <tomi.takussaari@nitor.fi>
2018-02-16 23:35:35 +0200

    fix link names

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,21 +3,20 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../findMyOp.css";
 import Link from "gatsby-link";
 
-const pages = [{path: "/", name: "Search"}, {path: "/branches", name: "Branches in map"}];
+const pages = [{path: "/", name: "Search for a branch"}, {path: "/branches", name: "Branches on a map"}];
 
 const Navi = () => {
     return <header className="container">
         <div className="row justify-content-center">
             <div className="col-12">
                 <ul className="list-inline">
-                    {pages.map((page, i) => {
-                        return <li className="list-inline-item">
+                    {pages.map((page, i) =>
+                        <li key={i} className="list-inline-item">
                             <Link to={page.path}>
                                 {page.name}
                             </Link>
                         </li>
-
-                    })}
+                    )}
                 </ul>
             </div>
         </div>
